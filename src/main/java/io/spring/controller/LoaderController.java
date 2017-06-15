37158db2467ebea1e470f651e33869dbdde4cc4b
Li Wang <liwang@pivotal.io>
2017-06-15 11:08:19 -0700

    optimized UI

--- src/main/java/io/spring/controller/LoaderController.java
+++ src/main/java/io/spring/controller/LoaderController.java
@@ -5,6 +5,9 @@ import java.net.URL;
 import java.util.*;
 import java.util.zip.GZIPInputStream;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.apache.geode.cache.Region;
 import org.apache.geode.cache.client.ClientCache;
 import org.apache.geode.cache.query.Query;
@@ -33,70 +36,64 @@ public class LoaderController {
 	@Autowired
     private SimpMessagingTemplate webSocket;
 	
-	Boolean start = null;
+	InputStream inputStream = null;
+	
+	BufferedReader reader = null;
+	
+	Boolean loading_flag = false;
+	
+	Boolean isLoading = false;
 	
 
 	@RequestMapping(method = RequestMethod.GET, path = "/start")
 	@ResponseBody
 	public String start(@RequestParam(value = "batch_size", required = true) String BATCH_SIZE) throws Exception {
 		Map<String, PdxInstance> buffer = new HashMap<>();
+	
+		loading_flag = true;
+		isLoading = true;
+		webSocket.convertAndSend("/topic/status", status());
 		
-		FileInputStream fis = null;
-		BufferedReader reader = null;
-		start = true;
+		String attributes = "ssn|first|last|gender|street|city|state|zip|latitude|longitude|city_pop|job|dob|account_num|profile|transaction_num|transaction_date|category|amount";
+		String[] keys = attributes.split("\\|");
 		
-		try {
-			URL fileLocation = new URL("https://s3.amazonaws.com/phd-sample-data/trans_fact.gz");
-			InputStream gzipStream = new GZIPInputStream(fileLocation.openStream());
-			reader = new BufferedReader(new InputStreamReader(gzipStream));
+		String line = reader.readLine();
+		int index = count()+1;
+		
+        while (line != null && loading_flag) {
+        	String[] values = line.split("\\|");
+        	Map<String, String> customerMap = new HashMap<>();
+        	
+        	if (keys.length != values.length) continue;
+        	
+            for (int i=0; i<keys.length; i++) {
+            	customerMap.put(keys[i], values[i]);
+            }
+            PdxInstance customer = JSONFormatter.fromJSON(new JSONObject(customerMap).toString());
 
-			String attributes = "ssn|first|last|gender|street|city|state|zip|latitude|longitude|city_pop|job|dob|account_num|profile|transaction_num|transaction_date|category|amount";
-			String[] keys = attributes.split("\\|");
-			
-			String line = reader.readLine();
-			int index = count()+1;
-			
-            while (line != null && start) {
-            	String[] values = line.split("\\|");
-            	Map<String, String> customerMap = new HashMap<>();
-            	
-            	if (keys.length != values.length) continue;
-            	
-                for (int i=0; i<keys.length; i++) {
-                	customerMap.put(keys[i], values[i]);
-                }
-                PdxInstance customer = JSONFormatter.fromJSON(new JSONObject(customerMap).toString());
-
-                buffer.put(UUID.randomUUID().toString(), customer);   
-                
-                if (index % 1000 == 0) {
-                	webSocket.convertAndSend("/topic/record_stats", index);
-                }
-                
-                if (index % Integer.parseInt(BATCH_SIZE) == 0) {
-                	transactionRegion.putAll(buffer);
-                	webSocket.convertAndSend("/topic/record_stats", index);
-                	buffer.clear();
-                }
-                
-                line = reader.readLine();
-                index++;
+            buffer.put(UUID.randomUUID().toString(), customer);   
+            
+            if (index % 1000 == 0) {
+            	webSocket.convertAndSend("/topic/record_stats", index);
             }
             
-            transactionRegion.putAll(buffer);
-            webSocket.convertAndSend("/topic/record_stats", count());
-			
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-            try {
-                if (reader != null) reader.close();
-                if (fis != null) fis.close();
-            } catch (Exception ex) {
-            	ex.printStackTrace();
+            if (index % Integer.parseInt(BATCH_SIZE) == 0) {
+            	transactionRegion.putAll(buffer);
+            	webSocket.convertAndSend("/topic/record_stats", index);
+            	buffer.clear();
             }
+            
+            line = reader.readLine();
+            index++;
+            
+            //Thread.sleep(50);
         }
+        
+        transactionRegion.putAll(buffer);
+        isLoading = false;
+        webSocket.convertAndSend("/topic/record_stats", count());
+        webSocket.convertAndSend("/topic/status", status());
+		
 
 		return "Successfully Loaded.";
 	}
@@ -106,15 +103,15 @@ public class LoaderController {
 	public String clear() throws Exception {
 		transactionRegion.removeAll(transactionRegion.keySetOnServer());
 		
-		return "Region cleared";
+		return "Region cleared.";
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, path = "/stop")
 	@ResponseBody
 	public String stop() throws Exception {
-		start = false;
+		loading_flag = false;
 		
-		return "Stopped.";
+		return "Stop Signal Emitted.";
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, path = "/count")
@@ -127,5 +124,25 @@ public class LoaderController {
 		
 		return (Integer) collection.iterator().next();
 	}
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/status")
+	@ResponseBody
+	public String status() throws Exception {
+		return isLoading ? "START" : "STOP";
+	}
+	
+	@PostConstruct
+	public void initReader() throws Exception {
+		URL FILE_LOCATION = new URL("https://s3.amazonaws.com/phd-sample-data/trans_fact.gz");
+		InputStream inputStream = new GZIPInputStream(FILE_LOCATION.openStream());
+		reader = new BufferedReader(new InputStreamReader(inputStream));
+	}
+	
+	@PreDestroy
+	public void closeReader() throws Exception {
+		if (reader != null) reader.close();
+		if (inputStream != null) inputStream.close();
+	}
+	
 	
 }
